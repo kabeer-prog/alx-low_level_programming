d3c2b31a5b5303c94ea86e269f1b4c3aecfd167d
kabeer-prog <kabeerlap@gmail.com>
2022-03-10 16:37:56 -0800

    4-put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
-*main - Entry point
-*First C file
-*Return: Always 0 (Success)
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming ia like building a multilingual puzzle");
+	return (0);
 }
