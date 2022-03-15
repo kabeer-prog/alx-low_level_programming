a46df735196445576aa9934bb51092b40772cd44
kabeer-prog <kabeerlap@gmail.com>
2022-03-15 04:01:34 -0700

    alphabet in lowercase and 0

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Print alphabets in lower case
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for  (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+
+	}
+	_putchar('\n');
+}
