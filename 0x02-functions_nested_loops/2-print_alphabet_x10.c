a36f6d7a8e16948daa81678405756133fa4c2c04
kabeer-prog <kabeerlap@gmail.com>
2022-03-15 04:44:36 -0700

    print alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,12 +6,12 @@
  * Return: Always 0;
  *
  */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	char i;
 	char h;
 
-	for (h = 1; h <= 10; h++)
+	for (h = 0; h <= 9; h++)
 	{
 		for (i = 'a'; i <= 'z'; i++)
 		{
