92b0f670bd0b08413db48acda3041e79ece7b502
kabeer-prog <kabeerlap@gmail.com>
2022-03-15 06:11:39 -0700

    Updated main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,4 +5,43 @@ int _putchar(char);
 void print_alphabet(void);
 
 
+void print_alphabet(void);
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ *
+ */
+
+int _islower(int c);
+/**
+ * _islower -prints if character is alphabet
+ * @c: character to use
+ * Return: 1 or 0
+ */
+
+int _isalpha(int c);
+/**
+ * print_sign - prints a sign based on n
+ * @n: integer to input
+ * Return: 1 or 0 or -1
+ */
+int print_sign(int n);
+/**
+ * _abs - gives absolute value
+ * @int: integer
+ * Return: integer or abs value of int
+ */
+int abs(int);
+
+int  print_last_digit(int);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int, int);
+
+void print_to_98(int n);
+
+void print_times_table(int n);
+
 #endif
