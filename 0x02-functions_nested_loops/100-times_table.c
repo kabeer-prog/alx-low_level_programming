c22b2403e61749d082748eb7292f2a7d2d492f8b
kabeer Akanni <77249754+kabeer-prog@users.noreply.github.com>
2022-03-16 04:10:17 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,50 +1,44 @@
 #include "main.h"
 
 /**
- * print_times_table - times table function
- * @n: integer to return
+ * print_times_table - prints the @n times table, starting with 0
  *
+ * @n: int
  */
 void print_times_table(int n)
 {
-	int i;
-	int j;
+	int a, b, c;
 
-	if (n <= 15 && n >= 0)
+	if (n > 15 || n < 0)
+		return;
+
+	for (a = 0; a <= n; a++)
 	{
-		for (i = 0; i <= n; i++)
+		_putchar(48);
+		for (b = 1; b <= n; b++)
 		{
-			for (j = 0; j <= n; j++)
+			c = a * b;
+			_putchar(',');
+			_putchar(' ');
+			if (c <= 9)
 			{
-				int prod = j * i;
-
-				if (j == 0)
-				{
-					_putchar('0');
-				} else if (prod <= 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar('0' + prod);
-				} else if (prod > 99)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar('0' + (prod / 100));
-					_putchar('0' + (prod / 10) % 10));
-					_putchar('0' + (prod % 10));
-				} else
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar('0' + (prod / 10));
-					_putchar('0' + (prod % 10));
-				}
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(c + '0');
 			}
-			_putchar('\n');
+			else if (c <= 99)
+			{
+				_putchar(' ');
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
+			}
+			else
+			{
+				_putchar(((c / 100) % 10) + '0');
+				_putchar(((c / 10) % 10) + '0');
+				_putchar((c % 10) + '0');
+			}
 		}
+		_putchar('\n');
 	}
 }
