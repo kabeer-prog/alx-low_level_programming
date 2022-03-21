e35ffe94337a447926632782e7c2d52d22be459c
kabeer Akanni <77249754+kabeer-prog@users.noreply.github.com>
2022-03-21 14:28:25 +0100

    create 101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _atoi - delete characters and let only numbers
+ * @s: string
+ * Return: integer
+ */
+int _keygen.c(char *s)
+{
+	int i, value, sign;
+
+
+	return (value);
+}
