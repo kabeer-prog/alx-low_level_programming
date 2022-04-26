41a4da81f3e780236212cff4e18bbda3518f9dec
kabeer Akanni <77249754+kabeer-prog@users.noreply.github.com>
2022-04-26 07:11:26 +0100

    create 2-add_nodeint.c

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+#include <stddef.h>
+
+/**
+ * *add_nodeint - function that adds a new node at the beginning of a
+ * singly linked list
+ * @head: doble pointer to the singly linked list
+ * @n: number to add to the singly linked list
+ * Return: address to the new element(pointer) or NULL if failed
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	if (head == NULL)
+		return (NULL);
+
+	new = malloc(sizeof(listint_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = *head;
+	*head = new;
+
+	return (*head);
+}
